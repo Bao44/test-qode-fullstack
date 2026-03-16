bbd1fa7ac95008c79cde9e51c2b284afadb4043d
Bao44 <tqbao44@gmail.com>
2026-03-16 17:37:26 +0700

    change url backend

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -4,13 +4,13 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
-export const getPhotos = () => api.get("/photos");
-export const uploadPhoto = (formData: any) => api.post("/photos", formData);
+export const getPhotos = () => api.get("/api/photos");
+export const uploadPhoto = (formData: any) => api.post("/api/photos", formData);
 export const addComment = (commentData: any) =>
-  api.post("/comments", commentData);
+  api.post("/api/comments", commentData);
 export const deletePhoto = (id: string, fileName: string) =>
-  api.delete(`/photos/${id}`, { data: { fileName } });
+  api.delete(`/api/photos/${id}`, { data: { fileName } });
 
-export const deleteComment = (id: number) => api.delete(`/comments/${id}`);
+export const deleteComment = (id: number) => api.delete(`/api/comments/${id}`);
 
 export default api;
